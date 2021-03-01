d7ee78b96914989c05c27e3f420a8e4d4298b368
tomppk <tomi.kivioja@gmail.com>
2021-03-01 10:48:11 +0200

    testing JSX vs. HTML

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
 // Create a react component
 const App = () => {
-    return <div>Hi there!</div>;
+const buttonText = {text: 'Click me!'}
+const style = {backgroundColor: 'blue', color: 'white'}
+const labelText = 'Enter name:'
+
+    return (
+<div>
+    <label className="label "htmlFor="name">{labelText}</label>
+    <input id="name" type="text"/>
+    <button style={style}>{buttonText.text}</button>
+</div>
+  )
 };
 
 
